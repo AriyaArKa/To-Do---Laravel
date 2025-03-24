2d56bf271c79265995140e049ffef1d4cd37ddd4
Arka Nath <nath2107055@stud.kuet.ac.bd>
2025-03-25 02:53:02 +0600

    button added 12:16

--- todos/resources/views/welcome.blade.php
+++ todos/resources/views/welcome.blade.php
@@ -3,3 +3,15 @@
     <title>To Do List App</title>
 
 @endpush
+
+
+
+@section('main-section')
+
+<div class="container">
+    <div class="d-flex jutify-content-between align-items-center my-5">
+        <div class="h2">All Todos</div>
+        <a href="" class="btn btn-primary btn-lg">Add Todo</a>
+    </div>
+</div>
+@endsection
