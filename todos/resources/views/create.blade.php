820a9cb1dd718ad1e7386a1e60a5a2386e1e3ba5
Arka Nath <nath2107055@stud.kuet.ac.bd>
2025-03-25 23:27:01 +0600

    validation done

--- todos/resources/views/create.blade.php
+++ todos/resources/views/create.blade.php
@@ -14,18 +14,39 @@
     </div>
 
 
+    {{-- {{ print_r($errors->all()) }} --}}
+
     <div class="card">
         <div class="card-body">
             <form action="{{ route("todo.store") }}" method="post">
+                
                 @csrf
+
                 <label for="" class="form-label mt-4">Name</label>
                 <input type="text" name="name" class="form-control">
+                <div class="text-danger">
+                    @error('name')
+                    {{ $message }}
+                    @enderror
+                </div>
+
 
                 <label for="" class="form-label mt-4">Work</label>
                 <input type="text" name="work" class="form-control">
+                <div class="text-danger">
+                    @error('work')
+                    {{ $message }}
+                    @enderror
+                </div>
+
 
                 <label for="" class="form-label mt-4">Due Date</label>
                 <input type="date" name="dueDate" class="form-control">
+                <div class="text-danger">
+                    @error('dueDate')
+                    {{ $message }}
+                    @enderror
+                </div>
 
                 <button class="btn btn-primary btn-lg mt-4">Add Todo</button>
 
